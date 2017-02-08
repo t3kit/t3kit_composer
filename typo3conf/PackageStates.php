61a27f3f1e55649d6fc2f0629cb2fad425336939
Man Math <manmath4@gmail.com>
2017-02-08 14:08:47 +0700

    [BUGFIX] Fix missing array end block in PackageStates (#15)

--- typo3conf/PackageStates.php
+++ typo3conf/PackageStates.php
@@ -352,6 +352,7 @@ return [
             'composerName' => 'helhum/typo3-console',
             'state' => 'active',
             'packagePath' => 'typo3conf/ext/typo3_console/',
+	    ],
         'url_forwarding' => [
             'composerName' => 'patrickbroens/url-forwarding',
             'state' => 'active',
